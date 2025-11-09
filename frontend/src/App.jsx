8456b5ed5327dc4418b8e45fbd9e6305301cf485
shivamparwate <shivamparvate61@gmail.com>
2025-11-09 15:06:28 +0530

    setup basic frontend folder structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,15 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home.jsx'
 
 const App = () => {
-  const [file, setFile] = useState(null);
-  const [message, setMessage] = useState('');
-
-  // Handle file selection
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  // Handle form submit
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!file) {
-      setMessage('Please select a file first.');
-      return;
-    }
-    const formData = new FormData();
-    formData.append('file', file);
-    try {
-      const response = await axios.post(
-        'http://localhost:5000/user/upload-file/resume',
-        formData,
-        {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            'Authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNoaXZhbXBhcnZhdGU2MUBnbWFpbC5jb20iLCJpYXQiOjE3NjI1MTE5MTMsImV4cCI6MTc2MjU5ODMxM30.KOXvfSgwDzRzPWAQ7GaJlL9Io-hmGy0PZdHF0OcYzKE"
-          }
-        }
-      );
-      setMessage('File uploaded successfully!');
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-      setMessage('Error uploading file.');
-    }
-  };
-
   return (
-    <div style={{ width: '400px', margin: '50px auto', textAlign: 'center' }}>
-      <form onSubmit={handleSubmit}>
-        <h2>Upload Resume</h2>
-        <input type="file" name="file" onChange={handleFileChange} />
-        <button type="submit">Upload</button>
-      </form>
-
-      {message && <p style={{ marginTop: '20px' }}>{message}</p>}
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
